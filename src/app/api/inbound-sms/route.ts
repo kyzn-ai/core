f53260613556a6d6a9772195ce6b8f12e92195e5
Riley Barabash <admin@rileybarabash.com>
2023-12-13 08:35:34 -0700

    feat: advertising response
    Customized inbound SMS response to log messages.

--- src/app/api/inbound-sms/route.ts
+++ src/app/api/inbound-sms/route.ts
@@ -18,8 +18,20 @@ export async function POST(req: NextRequest): Promise<NextResponse> {
         createMessage: ({ Body: content, From: sender }) => {
             //  Template response
 
-            return `Message received: "${content}" from ${sender}`
+            if (content.toLowerCase().trim() === "yes") {
+                console.log(`New opt-in from ${sender}`)
+                return "Thank you! I will send you an invite when it's ready."
+            } else {
+                console.log(`New inbound SMS from ${sender}: ${content}`)
+                return `Hey ${toTitleCase(content.toLowerCase().trim())}! My name is Riley, and I’m an independent software dev. I’m building an app called AMNESIA that will allow you to communicate with the future version of yourself via SMS (it’s powered by the world’s leading AI). My goal with AMNESIA is to help you reach your truest potential and achieve your goals by providing motivation, helpful insights, secondary accountability, and infinite knowledge at your fingertips.\n\nIt’s not finished yet, but when it is it will be free to get started.\n\nCan I send you an invite when it’s ready?\n\nReply with “YES” to confirm.`
+            }
         },
         debug: true
+    })
+}
+
+function toTitleCase(str: string) {
+    return str.replace(/\w\S*/g, function (txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
     })
 }
