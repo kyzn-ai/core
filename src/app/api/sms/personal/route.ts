ab52df09cb9adc87e3a863205c7f7ae190d0fab8
Riley Barabash <admin@rileybarabash.com>
2024-01-27 00:25:39 -0700

    feat: relay media (personal SMS)*
    Added functionality to allow forwarding media.

--- src/app/api/sms/personal/route.ts
+++ src/app/api/sms/personal/route.ts
@@ -3,6 +3,7 @@
  * @author Riley Barabash <riley@rileybarabash.com>
  */
 
+import { env } from "~/env"
 import { type DecodeParamsResult, composeResponse, decodeParams, sendMessage, validatePhone } from "~/lib/sms"
 import { type NextResponse, type NextRequest } from "next/server"
 
@@ -11,16 +12,23 @@ export async function POST(req: NextRequest): Promise<NextResponse> {
 
     const messageParams: DecodeParamsResult = await decodeParams({ req })
 
-    //  Define the numbers I will be receiving messages to + sending with
+    //  Define the numbers I will be receiving messages to and sending from
 
-    const myNumbers = ["+15873178927"]
+    //  TextNow / Text+
+
+    const myNumbers = ["+15873178927", "+17806663364"]
 
     //  If the incoming message is from me, forward it to the specified recipient
 
     if (myNumbers.includes(messageParams.From)) {
+        //  Gets the encoded media URL of an MMS, and sends it back to me
+
+        const rawEncodedMediaUrls: string[] | undefined = await getEncodedMediaUrls(messageParams)
+        if (rawEncodedMediaUrls) return composeResponse({ content: rawEncodedMediaUrls.join(", ") })
+
         //  Split the incoming SMS into components
 
-        const [recipient, message] = messageParams.Body.split(" | ")
+        const [recipient, message, encodedMediaUrls] = messageParams.Body.split(" | ")
 
         //  Validate the recipient
 
@@ -38,25 +46,40 @@ export async function POST(req: NextRequest): Promise<NextResponse> {
 
         //  Validate the message
 
-        if (!message) {
-            return composeResponse({ content: "Please specify the message using <NUMBER> | <MESSAGE>." })
-        }
+        // if (!message) {
+        //     return composeResponse({ content: "Please specify the message using <NUMBER> | <MESSAGE>." })
+        // }
+
+        //  Decode the media URLs
+
+        const mediaUrls: string[] | undefined = encodedMediaUrls ? encodedMediaUrls.split(", ").map(encodedMediaUrl => Buffer.from(encodedMediaUrl, "base64url").toString("utf-8")) : undefined
 
         //  Send the message
 
         await sendMessage({
             to: recipient,
             from: "+16672220221",
-            content: message
+            content: message ?? "",
+            mediaUrl: mediaUrls
         })
+
+        //  Generate a response and send it back to Twilio
+
+        return composeResponse({ content: "Delivered" })
     } else {
+        //  Get the media URLs if any
+
+        const rawEncodedMediaUrls: string[] | undefined = await getEncodedMediaUrls(messageParams)
+        const mediaUrls: string[] | undefined = rawEncodedMediaUrls ? rawEncodedMediaUrls.map(encodedMediaUrl => Buffer.from(encodedMediaUrl, "base64url").toString("utf-8")) : undefined
+
         //  If the incoming message is from someone else, forward it to each of my numbers
 
         for (const number of myNumbers) {
             await sendMessage({
                 to: number,
                 from: "+16672220221",
-                content: `${messageParams.From} | ${messageParams.Body}`
+                content: `${messageParams.From}${messageParams.Body && " | " + messageParams.Body}`,
+                mediaUrl: mediaUrls
             })
         }
     }
@@ -64,4 +87,35 @@ export async function POST(req: NextRequest): Promise<NextResponse> {
     //  Generate a response and send it back to Twilio
 
     return composeResponse({ content: "" })
+}
+
+async function getEncodedMediaUrls(messageParams: DecodeParamsResult): Promise<string[] | undefined> {
+    //  Check if the incoming message is an MMS
+
+    if (Number(messageParams.NumMedia) !== 0) {
+        //  Define a var for the URLs
+
+        const encodedMediaUrls: string[] = []
+
+        //  Loop over the media URLs
+
+        for (let index = 0; index < Number(messageParams.NumMedia); index++) {
+            //  Get the URL
+
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-member-access
+            const privateUrl: string = (messageParams as any)[`MediaUrl${index}`] as string
+
+            //  Get the public URL
+
+            const publicUrl = (await fetch(privateUrl, { headers: { Authorization: "Basic " + Buffer.from(env.TWILIO_SID + ":" + env.TWILIO_SECRET).toString("base64") } })).url
+
+            //  Encode the URL and push it to the array
+
+            encodedMediaUrls.push(Buffer.from(publicUrl).toString("base64url"))
+        }
+
+        //  Return the array
+
+        return encodedMediaUrls
+    } else return undefined
 }
